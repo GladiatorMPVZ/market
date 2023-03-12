81fc79c84b721ae7f7694f87cfa7df523b96c0cf
GladiatorMPVZ <inform.work981@mail.ru>
2023-03-12 21:23:13 +0300

    final

--- core-service/src/main/java/shopcore/services/ProductService.java
+++ core-service/src/main/java/shopcore/services/ProductService.java
@@ -2,6 +2,7 @@ package shopcore.services;
 
 import api.ProductDto;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import shopcore.entity.Product;
 import shopcore.repository.ProductRepository;
@@ -10,15 +11,27 @@ import java.util.List;
 
 @AllArgsConstructor
 @Service
+@Slf4j
 public class ProductService {
     private final ProductRepository productRepository;
+    private final IdentityMap identityMap;
 
     public List<Product> findAll() {
         return productRepository.findAll();
     }
 
     public Product findProductById(Long id) {
-        return productRepository.findById(id).orElseThrow();
+        Product product = this.identityMap.getProductMap(id);
+        if (product != null) {
+            return product;
+        } else {
+            product = productRepository.findById(id).orElseThrow();
+            if (product != null) {
+                this.identityMap.addProductMap(product);
+                return product;
+            }
+        }
+        return null;
     }
 
     public Product saveOrUpdateProduct(Product product) {
